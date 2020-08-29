010ad08078b3d32343b6c569be2a0c53cce2b3ab
Carlos <clockwerkz@gmail.com>
2020-08-29 15:04:36 -0700

    adds cors - client can ping backend routes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dashboard = (props) => {
+    const [msg, setMsg] = useState("Loading");
+    
+    useEffect(()=>{
+        fetch("http://localhost:4000/test")
+        .then(res => res.json())
+        .then(data => setMsg(data.msg))
+        .catch(err => console.log("error occured:", err));
+    }, [])
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <p>{msg}</p>
         </div>
     )
 }
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
 const app  = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send("Hello!"));
-app.get('/test', (req, res) => res.send("testing 123"));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.get('/', (req, res) => res.send({"msg":"Hello!"}));
+app.get('/test', (req, res) => res.send({"msg":"testing 123"}));
 
 
 app.listen(PORT);
