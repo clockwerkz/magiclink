a94c5866ab58eb91953cdffdff7a3cac7e3a33a5
Carlos <clockwerkz@gmail.com>
2020-08-30 15:51:16 -0700

    Adds PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/utilities/PrivateRoute';
+
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
@@ -11,7 +13,7 @@ function App() {
     <div className="App">
       <h1 className="text-center">Magic Link test</h1>
       <Router>
-        <Route path="/" exact component={Dashboard}/>
+        <PrivateRoute path="/" exact component={Dashboard}/>
         <Route path="/login" component={Login} />
       </Router>
     </div>
